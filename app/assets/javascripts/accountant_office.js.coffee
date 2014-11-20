79fce21538bd9f6232369d38af6e415ceed421ba
Torsten Ruger <torsten@villataika.fi>
2014-11-20 19:46:26 +0200

    convert to coffee

---
+++ app/assets/javascripts/accountant_office.js.coffee
@@ -0,0 +1,2 @@
+$ ->
+  $(".manage_first").html  '<div class="col-md-12"><a href="/manage/report"> Reports </a></div>'
--- app/assets/javascripts/accountant_office.js.rb
+++
@@ -1,4 +0,0 @@
-
-Document.ready? do
-  Element.find('.manage_first').html = '<div class="col-md-12"><a href="/manage/report"> Reports </a></div>'
-end
